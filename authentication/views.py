f3d059100cbcb303ed3ac7cb1558341fe58ee421
Brenda <brendagovha1@gmail.com>
2025-11-19 11:54:31 +0200

    implemented user authentication with JWT

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserRegistrationSerializer
+    
